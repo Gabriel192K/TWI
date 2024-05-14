cd738382c7e57604dab63502ebfe68e66fac66cf
Gabriel192K <103511655+Gabriel192K@users.noreply.github.com>
2024-05-14 12:43:48 +0300

    updated cpp file

--- TWI.cpp
+++ TWI.cpp
@@ -65,14 +65,6 @@ const uint8_t __TWI__::begin(void)
 }
 
 /*!
- * @brief  Begins the TWI implementation using a default frequency
- */
-void __TWI__::begin(void)
-{
-    this->begin(TWI_DEFAULT_FREQUENCY);
-}
-
-/*!
  * @brief  Setting the TWI bus communication frequency
  * @param  frequency
  *         The frequency of TWI bus communication
