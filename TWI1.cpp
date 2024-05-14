e8c57c0118655c97f5012fdac3d2a6053718a4cc
Gabriel192K <103511655+Gabriel192K@users.noreply.github.com>
2024-05-14 12:40:41 +0300

    updated TWI0 and TWI1 files

---
+++ TWI1.cpp
@@ -0,0 +1,13 @@
+/* Dependencies */
+#include "TWI.h"
+
+#if defined(HAS_TWI1)
+
+#elif defined(__AVR_ATmega328PB__)
+__TWI__ TWI1 = __TWI__(&TWBR1, &TWSR1, &TWAR1, &TWDR1, &TWCR1, &TWAMR1);
+ISR(TWI1_vect)
+{
+	TWI.isr();
+}
+#endif
+#endif
